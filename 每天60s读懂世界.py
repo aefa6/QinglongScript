9b8bddc81047bbb36e8214f32311359f3bf6d4f1
Sen <85537391+aefa6@users.noreply.github.com>
2023-07-15 12:27:58 +0800

    Update 每天60s读懂世界.py
    增加换行

--- 每天60s读懂世界.py
+++ 每天60s读懂世界.py
@@ -11,9 +11,10 @@ sc_key = '这里填Sever酱的push key'
 sc_url = f'https://sc.ftqq.com/{sc_key}.send'
 url = 'https://60s.viki.moe/?encoding=text'  # 来自知乎的一位大佬的接口
 resp = requests.get(url)
+news = resp.text.replace('\n', '\n      \n    ')
 
 info = f"""
-{resp.text}   
+{news}   
 """
 
 # Sever酱推送
