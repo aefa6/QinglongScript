fc44e182f47ecae98078f29b55aa80be358a1d83
Sen <85537391+aefa6@users.noreply.github.com>
2023-07-16 10:10:47 +0800

    Update 每天60s读懂世界.py

--- 每天60s读懂世界.py
+++ 每天60s读懂世界.py
@@ -1,14 +1,10 @@
 # @author Sten
 # 作者仓库:https://github.com/aefa6/QinglongScript.git
 # 觉得不错麻烦点个star谢谢
-
+# 使用青龙自带的通知
 import requests
 import json
-
-# Sever酱推送接口
-sc_key = '这里填Sever酱的push key' 
-# 下面的不用管了
-sc_url = f'https://sc.ftqq.com/{sc_key}.send'
+import notify
 url = 'https://60s.viki.moe/?encoding=text'  # 来自知乎的一位大佬的接口
 resp = requests.get(url)
 news = resp.text.replace('\n', '\n      \n    ')
@@ -17,5 +13,4 @@ info = f"""
 {news}   
 """
 
-# Sever酱推送
-requests.post(sc_url, data={'text': '60s读懂世界', 'desp': info})
+notify.send("每天60s读懂世界", info)
