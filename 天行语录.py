3badd9a57acb6244d9598325a0f97514344fec41
Sen <85537391+aefa6@users.noreply.github.com>
2023-07-29 14:03:53 +0800

    Update 天行语录.py

--- 天行语录.py
+++ 天行语录.py
@@ -6,15 +6,15 @@
 import requests
 import json
 import notify
-pyurl = 'https://apis.tianapi.com/pyqwenan/index?key={api}' #朋友圈文案
-hsurl = 'https://apis.tianapi.com/hsjz/index?key={api}' #失恋分手句子
-caurl = 'https://apis.tianapi.com/caihongpi/index?key={api}' #彩虹屁
-tiurl = 'https://apis.tianapi.com/tiangou/index?key={api}' #舔狗日志
-duurl = 'https://apis.tianapi.com/dujitang/index?key={api}' #毒鸡汤
-zhurl = 'https://apis.tianapi.com/zhanan/index?key={api}' #渣男语录
-zaurl = 'https://apis.tianapi.com/zaoan/index?key={api}' #早安心语
-waurl = 'https://apis.tianapi.com/wanan/index?key={api}' #晚安心语
-saurl = 'https://apis.tianapi.com/saylove/index?key={api}' #土味情话
+pyurl = f'https://apis.tianapi.com/pyqwenan/index?key={api}' #朋友圈文案
+hsurl = f'https://apis.tianapi.com/hsjz/index?key={api}' #失恋分手句子
+caurl = f'https://apis.tianapi.com/caihongpi/index?key={api}' #彩虹屁
+tiurl = f'https://apis.tianapi.com/tiangou/index?key={api}' #舔狗日志
+duurl = f'https://apis.tianapi.com/dujitang/index?key={api}' #毒鸡汤
+zhurl = f'https://apis.tianapi.com/zhanan/index?key={api}' #渣男语录
+zaurl = f'https://apis.tianapi.com/zaoan/index?key={api}' #早安心语
+waurl = f'https://apis.tianapi.com/wanan/index?key={api}' #晚安心语
+saurl = f'https://apis.tianapi.com/saylove/index?key={api}' #土味情话
 
 urls = [pyurl, tiurl, duurl] # 按照格式填写你要推送的内容对应的url，默认是朋友圈文案、舔狗日志和毒鸡汤
 api = "你的天行API key"
