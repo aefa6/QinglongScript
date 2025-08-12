1fea1d24a211f91ce54b8cee821ce4d0ceef3531
Sen <85537391+aefa6@users.noreply.github.com>
2025-08-12 19:16:40 +0800

    Update 每天60s读懂世界.py

--- 每天60s读懂世界.py
+++ 每天60s读懂世界.py
@@ -6,7 +6,7 @@ import requests
 import json
 import notify
 
-url = 'https://60s.viki.moe/v2/60s'
+url = 'https://60s.viki.moe/v2/60s?encoding=text'
 resp = requests.get(url)
 content = resp.text
 # 分片处理
